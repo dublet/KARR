c90a19ebbf53be0cf054ff2a4ff8f891da813b84
dublet <github@dublet.org>
2015-07-02 12:45:07 +0100

    Link with bgfx.

--- src/cmake/Modules/Findbgfx.cmake
+++ src/cmake/Modules/Findbgfx.cmake
@@ -1,8 +1,10 @@
+# Part of Project KARR
+# (c), released under BSD license
 
 find_path(bgfx_INCLUDE_DIR bgfx.h
           HINTS "../bgfx/include")
-find_path(bgfx_LIBRARY_DIR libbgfxRelease.a
+find_library(bgfx_LIBRARY NAMES bgfxRelease
           HINTS "../bgfx/.build/linux64_gcc/bin/")
 set(bgfx_INCLUDE_DIRS ${bgfx_INCLUDE_DIR} )
-set(bgfx_INCLUDE_DIRS "" )
+set(bgfx_LIBRARIES ${bgfx_LIBRARY} )
 mark_as_advanced(bgfx_INCLUDE_DIR bgfx_LIBRARY )
