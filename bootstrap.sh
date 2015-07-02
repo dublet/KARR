f8e69d53838ff5395c6e70bb22e414b8daaa5f55
dublet <github@dublet.org>
2015-07-02 11:29:13 +0100

    Add necessary bgfx components and a script to set everything up for KARR.

---
+++ bootstrap.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Part of Project KARR
+# (c), released under the BSD license.
+
+STARTDIR=${PWD}
+function failed() {
+    echo "Failed to build a required component, exiting..;";
+    cd ${STARTDIR};
+    exit 1;
+}
+
+if [ -d genie ]; then
+    (cd genie && make) || failed;
+
+    cd ../bgfx/tools/bin/linux;
+    if [ ! -L genie ]; then
+	ln -vs ../../../../genie/bin/linux/genie .;
+    fi
+    cd ${STARTDIR};
+fi
+if [ -d bx ]; then
+    (cd bx && make) || failed;
+    cd ${STARTDIR};
+fi
+
+if [ -d bgfx ] ; then
+    (cd bgfx && make linux-release64 linux-debug64) || failed;
+    cd ${STARTDIR};
+fi
