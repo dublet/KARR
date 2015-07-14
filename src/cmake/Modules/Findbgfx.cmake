65f23a7d1bfa844727ee79409e744e6a22d7ffcb
dublet <github@dublet.org>
2015-07-14 18:16:11 +0100

    Add bx include

--- src/cmake/Modules/Findbgfx.cmake
+++ src/cmake/Modules/Findbgfx.cmake
@@ -1,10 +1,12 @@
 # Part of Project KARR
 # (c), released under BSD license
 
+find_path(bx_INCLUDE_DIR bx/bx.h
+          HINTS "../bx/include")
 find_path(bgfx_INCLUDE_DIR bgfx.h
           HINTS "../bgfx/include")
 find_library(bgfx_LIBRARY NAMES bgfxRelease
           HINTS "../bgfx/.build/linux64_gcc/bin/")
-set(bgfx_INCLUDE_DIRS ${bgfx_INCLUDE_DIR} )
+set(bgfx_INCLUDE_DIRS ${bgfx_INCLUDE_DIR} ${bx_INCLUDE_DIR} )
 set(bgfx_LIBRARIES ${bgfx_LIBRARY} )
 mark_as_advanced(bgfx_INCLUDE_DIR bgfx_LIBRARY )
