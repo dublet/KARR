baddd2c1c97efd81bfab31b9dbc8661937dbf16d
dublet <github@dublet.org>
2015-07-22 12:38:12 +0100

    Change sInstruments to contains pointers.

--- src/main.cpp
+++ src/main.cpp
@@ -67,7 +67,7 @@ static const int w = 1024;
 static const int h = 600;
 static SDL_Window *sSdlWindow = nullptr;
 static SerialConnection sArduinoConnection;
-static std::vector<Instrument> sInstruments;
+static std::vector<Instrument *> sInstruments;
 
 void handleKeyboard(unsigned char key, int x, int y) {
     if (key == 27)
@@ -97,8 +97,9 @@ void handleDisplay() {
     bgfx::dbgTextPrintf(0, 1, 0x4f, "bgfx/examples/00-helloworld");
     bgfx::dbgTextPrintf(0, 2, 0x6f, "Description: Initialization and debug text.");
 
-    for (Instrument &instrument : sInstruments) {
-	instrument.draw();
+    for (Instrument *instrument : sInstruments) {
+	if (instrument)
+	    instrument->draw();
     }
     // Advance to next frame. Rendering thread will be kicked to
     // process submitted rendering primitives.
